31f3eed31c7b19d9cc7cdf264c77a4080cc3d053
ErikWulms <e.wulms@blue-engineering.nl>
2018-10-12 16:27:21 +0200

    Manage pumps uitbreiden met radio buttons

--- Carbro/Screens/Screen_ManagePumps.xaml.cs
+++ Carbro/Screens/Screen_ManagePumps.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +23,29 @@ namespace Carbro.Screens
     /// </summary>
     public sealed partial class ManagePumps : Page
     {
+        bool Manual_Toggle = true;
         public ManagePumps()
         {
             this.InitializeComponent();
+        }
+
+        private void Manual_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Manual_Button.Background = new SolidColorBrush(Colors.Green);
+            Calibrate_Button.Background = new SolidColorBrush(Colors.Gray);
+            Manual_Toggle = true;
+        }
+
+        private void Calibrate_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Manual_Button.Background = new SolidColorBrush(Colors.Gray);
+            Calibrate_Button.Background = new SolidColorBrush(Colors.Green);
+            Manual_Toggle = false;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
